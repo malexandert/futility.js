e5fc2ed7f23057fcf2e3d6e7c49a654c74d8023c
Marcus Todd <matodd@andrew.cmu.edu>
2015-12-20 18:46:19 -0500

    Generalized gallery pages. Added page for gradients

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,24 +1,33 @@
 package controllers
 
+import models.GalleryProperties._
+
 import play.api._
 import play.api.mvc._
 
 class Application extends Controller {
 
-  def futility = Action {
-    Ok(views.html.futility("futility"))
-  }
-
   def main = Action {
     Ok(views.html.main("art"))
   }
 
+  def futility = Action {
+    Ok(views.html.futility("futility"))
+  }
+
   def infoloss = Action {
-    Ok(views.html.infoloss("information loss"))
+    Ok(views.html.gallery(infolossPageTitle, infolossSectionTitle, infolossBody,
+      infolossImagePrefix, infolossNumImages))
+  }
+
+  def gradients = Action {
+    Ok(views.html.gallery(gradientsPageTitle, gradientsSectionTitle, gradientsBody,
+      gradientsImagePrefix, gradientsNumImages))
   }
 
   def solvethemall = Action {
-    Ok(views.html.solvethemall("solve them all"))
+    Ok(views.html.gallery(solvethemallPageTitle, solvethemallSectionTitle, solvethemallBody,
+      solvethemallImagePrefix, solvethemallNumImages))
   }
 
 }
